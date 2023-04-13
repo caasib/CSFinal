4e96ffa41fea8e81f3a3a3620e959049bd17925e
caasib <nketiah2005@gmail.com>
2023-04-13 11:14:59 -0400

    jrthkjerngjn

--- src/main/java/com/example/csfinal/Application.java
+++ src/main/java/com/example/csfinal/Application.java
@@ -1,17 +1,50 @@
 package com.example.csfinal;
 
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import java.util.*;
 
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+    int columns = 10, rows = 3, horizontal = 100, vertical = 100;
+    int breakableHorizontalMin = 45, breakableHorizontalMax = 90, breakableVertical = 15;
+    Random rand = new Random();
+
+    public Node[] addBreakableRectangles() {
+        Node[] groupOfBreakables = new Node[columns];
+        for (int i = 0; i < columns; i++) {
+            groupOfBreakables[i] = new Rectangle(rand.nextInt(breakableHorizontalMin, breakableHorizontalMax), breakableVertical);
+        }
+        return groupOfBreakables;
+    }
+
+    public GridPane createBreakables() {
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(3);
+        for (int i = 0; i < rows; i++) {
+            gridPane.add(new HBox(5, addBreakableRectangles()), 0, i);
+        }
+        return gridPane;
+    }
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Really bad candy crush");
+    public void start(Stage stage) {
+        AnchorPane root = new AnchorPane();
+        GridPane gridPane = createBreakables();
+
+        gridPane.add(new Rectangle(300, 20), 0, 50);
+
+        AnchorPane.setLeftAnchor(gridPane, 0.0);
+        AnchorPane.setRightAnchor(gridPane, 0.0);
+
+        root.getChildren().add(gridPane);
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Atari Breakout");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/csfinal/Controller.java
+++ src/main/java/com/example/csfinal/Controller.java
@@ -17,32 +17,4 @@ public class Controller {
     @FXML
     AnchorPane pane;
 
-    Random rand = new Random();
-
-    Image redTriangle = new Image(Objects.requireNonNull(getClass().getResourceAsStream("red.png")));
-    Image blueCircle = new Image(Objects.requireNonNull(getClass().getResourceAsStream("blue.png")));
-    Image greenSquare = new Image(Objects.requireNonNull(getClass().getResourceAsStream("green.png")));
-    Image yellowStar = new Image(Objects.requireNonNull(getClass().getResourceAsStream("yellow.png")));
-
-    Image[] images = {redTriangle, blueCircle, greenSquare, yellowStar};
-
-    public boolean nextTo(ImageView imageView1, ImageView imageView2) {
-        ObservableList<Node> imageList = pane.getChildren();
-        return (imageList.indexOf(imageView1) + 1 == imageList.indexOf(imageView2)) || (imageList.indexOf(imageView1) - 1 == imageList.indexOf(imageView2));
-    }
-
-    public void selectImage(ImageView imageView) {
-        ObservableList<Node> imageList = pane.getChildren();
-        for (int i = 0; i < imageList.size(); i++) {
-            if (nextTo((ImageView)imageList.get(i), imageView)) {
-                System.out.println("these are next to each other: " + ((ImageView) imageList.get(i)).getImage() + " and " + imageView.getImage());
-            }
-        }
-    }
-
-    public void imageClick(MouseEvent e) {
-        ImageView currentImage = (ImageView)e.getSource();
-        selectImage(currentImage);
-        //currentImage.setImage(images[rand.nextInt(images.length)]);
-    }
 }
