5a673ee2f47b1c4664499d4e2793004fce4a5356
caasib <107368103+caasib@users.noreply.github.com>
2023-04-17 00:24:02 -0400

    hbg cyxtxdxd

--- src/main/java/com/example/csfinal/Application.java
+++ src/main/java/com/example/csfinal/Application.java
@@ -10,24 +10,31 @@ import java.util.*;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
-    int columns = 10, rows = 3, horizontal = 100, vertical = 100;
-    int breakableHorizontalMin = 45, breakableHorizontalMax = 90, breakableVertical = 15;
+    int columns = 10, rows = 3, horizontal = 10, vertical = 10;
+    int breakableHorizontal = 10, breakableVertical = 10;
     Random rand = new Random();
 
     public Node[] addBreakableRectangles() {
         Node[] groupOfBreakables = new Node[columns];
         for (int i = 0; i < columns; i++) {
-            groupOfBreakables[i] = new Rectangle(rand.nextInt(breakableHorizontalMin, breakableHorizontalMax), breakableVertical);
+            groupOfBreakables[i] = new Rectangle(breakableHorizontal, breakableVertical);
         }
         return groupOfBreakables;
     }
 
     public GridPane createBreakables() {
         GridPane gridPane = new GridPane();
+        Rectangle rect;
         gridPane.setVgap(3);
-        for (int i = 0; i < rows; i++) {
-            gridPane.add(new HBox(5, addBreakableRectangles()), 0, i);
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                rect = new Rectangle(c, r, breakableHorizontal, breakableVertical);
+                gridPane.getChildren().add(rect);
+            }
         }
+//        for (int i = 0; i < rows; i++) {
+//            gridPane.add(new HBox(5, addBreakableRectangles()), 0, i);
+//        }
         return gridPane;
     }
 
@@ -36,7 +43,7 @@ public class Application extends javafx.application.Application {
         AnchorPane root = new AnchorPane();
         GridPane gridPane = createBreakables();
 
-        gridPane.add(new Rectangle(300, 20), 0, 50);
+        gridPane.add(new Rectangle(120, 20), 1, 50);
 
         AnchorPane.setLeftAnchor(gridPane, 0.0);
         AnchorPane.setRightAnchor(gridPane, 0.0);
