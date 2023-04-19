228fd3dafd54b66d4853783ee7cd92608c0dc357
caasib <nketiah2005@gmail.com>
2023-04-19 17:08:35 -0400

    TODO: animation :3

--- src/main/java/com/example/csfinal/Application.java
+++ src/main/java/com/example/csfinal/Application.java
@@ -1,8 +1,15 @@
 package com.example.csfinal;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import java.util.*;
@@ -10,53 +17,32 @@ import java.util.*;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
-    int columns = 10, rows = 3, horizontal = 10, vertical = 10;
-    int breakableHorizontal = 10, breakableVertical = 10;
-    Random rand = new Random();
-
-    public Node[] addBreakableRectangles() {
-        Node[] groupOfBreakables = new Node[columns];
-        for (int i = 0; i < columns; i++) {
-            groupOfBreakables[i] = new Rectangle(breakableHorizontal, breakableVertical);
-        }
-        return groupOfBreakables;
-    }
-
-    public GridPane createBreakables() {
-        GridPane gridPane = new GridPane();
-        Rectangle rect;
-        gridPane.setVgap(3);
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < columns; c++) {
-                rect = new Rectangle(c, r, breakableHorizontal, breakableVertical);
-                gridPane.getChildren().add(rect);
-            }
-        }
-//        for (int i = 0; i < rows; i++) {
-//            gridPane.add(new HBox(5, addBreakableRectangles()), 0, i);
-//        }
-        return gridPane;
-    }
 
     @Override
-    public void start(Stage stage) {
-        AnchorPane root = new AnchorPane();
-        GridPane gridPane = createBreakables();
-
-        gridPane.add(new Rectangle(120, 20), 1, 50);
-
-        AnchorPane.setLeftAnchor(gridPane, 0.0);
-        AnchorPane.setRightAnchor(gridPane, 0.0);
-
-        root.getChildren().add(gridPane);
-
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
         Scene scene = new Scene(root);
         stage.setTitle("Atari Breakout");
         stage.setScene(scene);
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent e) {
+                switch (e.getCode()) {
+                    case A:
+                    case D:
+                        controller.move(e);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/com/example/csfinal/Controller.java
+++ src/main/java/com/example/csfinal/Controller.java
@@ -1,20 +1,37 @@
 package com.example.csfinal;
 
-import javafx.collections.ObservableList;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-
-import java.util.Random;
-
-import java.util.Objects;
+import javafx.scene.control.Dialog;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class Controller {
     @FXML
-    AnchorPane pane;
+    Rectangle playerRectangle;
+    double x;
+    Circle playBall;
+
+    public void move(KeyEvent e) {
+        switch (e.getCode()) {
+            case A:
+                if ((x - 5) >= -(playerRectangle.getParent().getLayoutBounds().getWidth()/3)) {
+                    playerRectangle.setX(x -= 5);
+                }
+                break;
+            case D:
+                if ((x + 5) <= (playerRectangle.getParent().getLayoutBounds().getWidth()/3)) {
+                    playerRectangle.setX(x += 5);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    //Timeline loop = new Timeline()
 
 }
