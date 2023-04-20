fd4a04095baf04c9f6cc6c7e9a4efe250bb7504f
caasib <107368103+caasib@users.noreply.github.com>
2023-04-20 01:28:39 -0400

    more animation ewjbejfbwhfb

--- src/main/java/com/example/csfinal/Controller.java
+++ src/main/java/com/example/csfinal/Controller.java
@@ -1,15 +1,26 @@
 package com.example.csfinal;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.scene.Scene;
 import javafx.scene.control.Dialog;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     @FXML
     Rectangle playerRectangle;
     double x;
@@ -17,15 +28,18 @@ public class Controller {
     @FXML
     Circle playBall;
 
+    @FXML
+    AnchorPane pane;
+
     public void move(KeyEvent e) {
         switch (e.getCode()) {
             case A:
-                if ((x - 5) >= -(playerRectangle.getParent().getLayoutBounds().getWidth()/3)) {
+                if ((x + 5) >= -(playerRectangle.getParent().getLayoutBounds().getWidth() / 3)) {
                     playerRectangle.setX(x -= 5);
                 }
                 break;
             case D:
-                if ((x + 5) <= (playerRectangle.getParent().getLayoutBounds().getWidth()/3)) {
+                if ((x + 5) <= (playerRectangle.getParent().getLayoutBounds().getWidth() / 3)) {
                     playerRectangle.setX(x += 5);
                 }
                 break;
@@ -34,6 +48,43 @@ public class Controller {
         }
     }
 
-    //Timeline loop = new Timeline()
+    /* Sources for ball movement:
+    https://www.youtube.com/watch?v=x6NFmzQHvMU
+    https://gamedev.stackexchange.com/questions/4253/in-pong-how-do-you-calculate-the-balls-direction-when-it-bounces-off-the-paddl?noredirect=1&lq=1
+    */
 
+    Timeline bounce = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
+        //Amount that the ball will move per frame
+        //This is one frame per 10 milliseconds which is 100 FPS
+        int deltaX = 3;
+        int deltaY = 3;
+
+        @Override
+        public void handle(ActionEvent e) {
+            //The movement of the ball per frame
+            playBall.setLayoutX(playBall.getLayoutX() + deltaX);
+            playBall.setLayoutY(playBall.getLayoutY() + deltaY);
+
+            //Finding the borders of the AnchorPane so that the ball can bounce off of them instead of leaving the frame
+            Bounds bounds = pane.getBoundsInLocal();
+            boolean rightBorder = playBall.getLayoutX() >= (bounds.getMaxX() - playBall.getRadius());
+            boolean leftBorder = playBall.getLayoutX() <= (bounds.getMinX() + playBall.getRadius());
+            boolean topBorder = playBall.getLayoutY() >= (bounds.getMaxY() - playBall.getRadius());
+            boolean bottomBorder = playBall.getLayoutY() <= (bounds.getMinY() + playBall.getRadius());
+
+            //If at one of the borders, change direction accordingly
+            if (rightBorder || leftBorder) {
+                deltaX *= -1;
+            }
+            if (topBorder || bottomBorder) {
+                deltaY *= -1;
+            }
+        }
+    }));
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        bounce.setCycleCount(-1); //Equivalent to Animation.INDEFINITE
+        bounce.play();
+    }
 }
